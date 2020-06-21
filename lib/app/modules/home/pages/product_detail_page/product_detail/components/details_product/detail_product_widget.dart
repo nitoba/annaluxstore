752b5948e47d997f9efcf44210d4e66182b5b43e
Bruno Santos <brukum2@gmail.com>
2020-06-21 03:32:52 -0300

    fix grey screen in product detail release mode

--- lib/app/modules/home/pages/product_detail_page/product_detail/components/details_product/detail_product_widget.dart
+++ lib/app/modules/home/pages/product_detail_page/product_detail/components/details_product/detail_product_widget.dart
@@ -18,84 +18,83 @@ class DetailProduct extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height / 2.15),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(32),
-            topRight: Radius.circular(32),
-          ),
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height / 2.15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(32),
+          topRight: Radius.circular(32),
         ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 32),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    title,
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  IconButton(
-                    icon: FaIcon(FontAwesomeIcons.heart),
-                    onPressed: () {},
-                  )
-                ],
-              ),
-              SizedBox(height: 6),
-              Text(
-                categorie,
-                style: TextStyle(
-                  fontSize: 14,
-                ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: thirdColor.withOpacity(0.3),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Text(
-                  "R\$ $price",
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  title,
                   style: TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
+                IconButton(
+                  icon: FaIcon(FontAwesomeIcons.heart),
+                  onPressed: () {},
+                )
+              ],
+            ),
+            SizedBox(height: 6),
+            Text(
+              categorie,
+              style: TextStyle(
+                fontSize: 14,
               ),
-              SizedBox(height: 32),
-              Text(
-                "Descrição:",
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: thirdColor.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Text(
+                "R\$ $price",
                 style: TextStyle(
-                  fontSize: 18,
+                  fontSize: 24,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 12),
-              Text(
-                description,
-                style: TextStyle(
-                  fontSize: 16,
-                ),
+            ),
+            SizedBox(height: 32),
+            Text(
+              "Descrição:",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
-              Spacer(),
-              ButtonWidget(
-                title: "Comprar",
-                icon: FontAwesomeIcons.shoppingBag,
-                onPress: () {},
+            ),
+            SizedBox(height: 12),
+            Text(
+              description,
+              style: TextStyle(
+                fontSize: 16,
               ),
-            ],
-          ),
+            ),
+            Spacer(),
+            ButtonWidget(
+              title: "Comprar",
+              icon: FontAwesomeIcons.shoppingBag,
+              onPress: () {},
+            ),
+          ],
         ),
       ),
     );
